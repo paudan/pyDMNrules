4423110dd570055edd0c3b32b3d2841bea688155
paulius <danpaulius@gmail.com>
2020-06-12 12:26:12 +0300

    fix minimum openpyxl requirements

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    install_requires=['datetime', 'pySFeel','openpyxl'],
+    install_requires=['datetime', 'pySFeel','openpyxl>3.0'],
 )
 
